81bdced3569e14ff1b9d1e39ede90f07bbe31035
Knotai <mnareff@gmail.com>
2016-01-29 00:29:30 +0000

    Create user show listing and styleize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,22 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
   
   def create
-   @user = User.new(user_params)
-   if @user.save
-     flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-     redirect_to articles_path
-   else
-     render "new"
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render "new"
+    end
   end
-end
 
 def edit
   @user = User.find(params[:id])
